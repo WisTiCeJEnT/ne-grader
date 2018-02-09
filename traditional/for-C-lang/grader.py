2d5944b12f4f127c509a6b1634ae005eb0b607c5
Wattanai Sathuphan <nine.stay27@gmail.com>
2018-02-09 22:16:27 +0700

    ready for real&more test case :)

--- traditional/for-C-lang/grader.py
+++ traditional/for-C-lang/grader.py
@@ -7,5 +7,16 @@ os.system(f"gcc {f}.c -o {f}.out")
 os.system(f"mv {f}.out {f}/")
 config = open(f"gcf/{f}.gcf",'r').read().split()
 numOfTestCase = int(config[0])
+score = 0
 for i in range(1,numOfTestCase+1):
     os.system(f"{f}/{f}.out < testcase/{f}/{i}.in > {f}/{i}.ans")
+for i in range(1,numOfTestCase+1):
+    sol = open(f"testcase/{f}/{i}.sol").read()
+    ans = open(f"{f}/{i}.ans").read()
+    if sol==ans:
+        print("P",end='')
+        score += 1
+    else:
+        print("-",end='')
+print(f"\nScore = {score}/{numOfTestCase}")
+os.system(f"rm -r {f}/")
