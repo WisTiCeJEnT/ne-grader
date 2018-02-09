5b844af612ac2dbffcbab7027c0bf44c44144306
Wattanai Sathuphan <nine.stay27@gmail.com>
2018-02-09 20:48:22 +0700

    answer is ready for compare with .sol

--- traditional/for-C-lang/grader.py
+++ traditional/for-C-lang/grader.py
@@ -5,5 +5,7 @@ f = f[0:-2]
 os.system(f"mkdir {f}/")
 os.system(f"gcc {f}.c -o {f}.out")
 os.system(f"mv {f}.out {f}/")
-config = open(f"gcf/{f}.gcf",'r').read()
-print(config)
+config = open(f"gcf/{f}.gcf",'r').read().split()
+numOfTestCase = int(config[0])
+for i in range(1,numOfTestCase+1):
+    os.system(f"{f}/{f}.out < testcase/{f}/{i}.in > {f}/{i}.ans")
